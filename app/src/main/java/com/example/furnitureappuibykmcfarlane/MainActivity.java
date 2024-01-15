1b48c2f09ed3db7f9d731daa90ccc611ae8f7da4
keneil mcfarlane <kenielmcfarlane73@gmail.com>
2024-01-15 17:42:30 -0500

    Your commit message

---
+++ app/src/main/java/com/example/furnitureappuibykmcfarlane/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.furnitureappuibykmcfarlane;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.furnitureappuibykmcfarlane.Main2Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
+        });
+    }
+}
